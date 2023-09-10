c9c65ae9b26db67e4017c41c46ac7ce228339e8c
Alessio Parmeggiani <parmeggiani.alessio1@gmail.com>
2023-09-10 10:21:18 +0200

    new firmware for structure

---
+++ firmware_struttura_esp/src/pinmap.h
@@ -0,0 +1,47 @@
+/*
+DRV_X_Y_Z:
+X= FRONT(F), REAR(R), MIDDLE(M)
+Y= LEFT(L), RIGHT(R)
+Z=CW CCW S (Speed)
+*/
+//front
+#define DRV_F_L_CW 23
+#define DRV_F_L_CCW 22
+#define DRV_F_L_IDX 1
+
+
+#define DRV_F_R_CW 27
+#define DRV_F_R_CCW 26
+#define DRV_F_R_IDX 2
+
+//rear
+#define DRV_R_L_CW 35
+#define DRV_R_L_CCW 34
+#define DRV_R_L_IDX 3
+
+#define DRV_R_R_CW 39
+#define DRV_R_R_CCW 38
+#define DRV_R_R_IDX 4
+
+//middle
+#define DRV_M_L_CW 29
+#define DRV_M_L_CCW 28
+#define DRV_M_L_IDX 5
+
+#define DRV_M_R_CW 33
+#define DRV_M_R_CCW 32
+#define DRV_M_R_IDX 6
+
+//front
+#define STEER_F_L_S 3
+#define STEER_F_L_D 2
+
+#define STEER_F_R_S 7
+#define STEER_F_R_D 6
+
+//rear
+#define STEER_R_L_S 9
+#define STEER_R_L_D 8
+
+#define STEER_R_R_S 10
+#define STEER_R_R_D 11
